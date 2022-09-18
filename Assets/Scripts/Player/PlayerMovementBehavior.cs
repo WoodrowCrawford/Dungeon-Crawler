3590d29021b727b1e8bf47b00da071b04c2707ee
Woodrow Crawford <woodrowcrawford03@gmail.com>
2022-09-18 00:49:40 -0500

    Added character texture and refined movement joystick

--- Assets/Scripts/Player/PlayerMovementBehavior.cs
+++ Assets/Scripts/Player/PlayerMovementBehavior.cs
@@ -4,48 +4,57 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
+
+
+
 public class PlayerMovementBehavior : MonoBehaviour
 {
+    //the rigidbody for the player
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
-
+    
+    //the input actions 
     private PlayerInputActions inputActions;
 
     [SerializeField]
     private float _speed = 3f;
 
+    //Gets the components
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+   
         inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
+
 
         //Subscribes to the move event
         inputActions.Player.Move.performed += Move;
     }
 
 
- 
     private void FixedUpdate()
     {
         Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
         rb.velocity = inputVector * _speed;
 
-
-        
-       
+        //if the vector is greater than 0 (moving forward)
+        if (inputVector.x > 0f)
+        {
+            //make the character face right
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        //if the vector is less than 0 (moving backwards)
+        else if(inputVector.x < 0f)
+        {
+            //make the character face left
+            transform.localScale = new Vector2(-1f, 1f);
+        }
     }
 
     //How the player moves
     public void Move(InputAction.CallbackContext context)
     {
-
-       // Debug.Log(context.ReadValue<Vector2>());
         Vector2 inputVector = context.ReadValue<Vector2>();
-        
-
-        // rb.AddForce(new Vector2(inputVector.x, inputVector.y) * speed, ForceMode2D.Force);
     }
 
 }
