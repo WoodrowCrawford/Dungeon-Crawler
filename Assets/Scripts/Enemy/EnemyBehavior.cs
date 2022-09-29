dcd3a739d36d99d249c9002c03312bede1a9599f
Woodrow Crawford <woodrowcrawford03@gmail.com>
2022-09-29 18:39:16 -0500

    Finished up enemy ai, just need to do some code cleaning

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
 
 
 public class EnemyBehavior : MonoBehaviour
@@ -17,12 +17,10 @@ public class EnemyBehavior : MonoBehaviour
         CHASE
     }
 
-    [SerializeField]
-    private Rigidbody2D _rb2d;
-
-    [SerializeField]
     [Header("Enemy Name")]
-    private string _name;
+    [SerializeField] private string _name;
+
+    SpriteRenderer _enemySprite;
 
 
     [Header("EnemyStats")]
@@ -33,50 +31,83 @@ public class EnemyBehavior : MonoBehaviour
 
 
     [Header("Current Enemy State")]
-    [SerializeField]
-    private EnemyState _currentState;
-
-
+    [SerializeField] private EnemyState _currentState;
+  
 
     [Header("Chasing State Variables")]
-    [SerializeField] private Transform[] _moveSpots;
-    
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _dectectionRange;
 
+
+    [Header("Patrol State Variables")]
+    [SerializeField] private Transform[] _moveSpots;
+    [SerializeField] private int _randomSpot;
+    [SerializeField] private float _waitTime;
+    [SerializeField] private float _startWaitTime;
+
+    [Header("Wander State Variables")]
+    [SerializeField] private Transform _moveSpot;
+    [SerializeField] private float _minX;
+    [SerializeField] private float _minY;
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _maxY;
 
 
     private void Awake()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
+        _enemySprite = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         _currentHealth = _maxHealth;
-       
+        _waitTime = _startWaitTime;
     }
 
 
     private void Update()
     {
-        //Tests to see what the current state is
-        Debug.Log(_currentState.ToString());
-
-        //A state machine used to switch between AI states
         switch (_currentState)
         {
+            //The idle state for the enemy
             case EnemyState.IDLE:
+                
                 {
-                   if(Keyboard.current.digit1Key.wasPressedThisFrame)
+                    //If the player is in range of the enemy
+                    if(Vector2.Distance(transform.position, _target.position) < _dectectionRange)
                     {
-                        _currentState = EnemyState.WANDER;
-                    }
+                        Debug.Log("Player is in range");
 
+                        //Change to chase state
+                        _currentState = EnemyState.CHASE;
+                    }                 
                     break;
                 }
 
+                //The enemy wanders randomly in this state
             case EnemyState.WANDER:
                 {
+                    transform.position = Vector2.MoveTowards(transform.position, _moveSpot.position, _speed * Time.deltaTime);
 
+
+                    //If the player is in range of the enemy
+                    if (Vector2.Distance(transform.position, _target.position) < _dectectionRange)
+                    {
+                        Debug.Log("Player is in range");
+
+                        //Change to chase state
+                        _currentState = EnemyState.CHASE;
+
+                        //Change enemy color
+                        _enemySprite.color = Color.red;
+                    }
+
+
+
+
+
+
+                    //Debug purposes
                     if (Keyboard.current.digit1Key.wasPressedThisFrame)
                     {
                         _currentState = EnemyState.PATROL;
@@ -84,25 +115,92 @@ public class EnemyBehavior : MonoBehaviour
 
 
 
+
+                    if (Vector2.Distance(transform.position, _moveSpot.position) < 0.2f)
+                    {
+                        if (_waitTime <= 0)
+                        {
+                            _moveSpot.position = new Vector2(UnityEngine.Random.Range(_minX, _maxX), UnityEngine.Random.Range(_minY, _maxY));
+                            _waitTime = _startWaitTime;
+                        }
+                        else
+                        {
+                            _waitTime -= Time.deltaTime;
+                        }
+
+
+                    }
+
+
+
+
+
                     break;
                 }
 
-
+               
+                //This is when the enemy wants to patrol an area 
             case EnemyState.PATROL:
                 {
+                   
+                    transform.position = Vector2.MoveTowards(transform.position, _moveSpots[_randomSpot].position, _speed * Time.deltaTime);
 
+
+                    //If the player is in range of the enemy
+                    if (Vector2.Distance(transform.position, _target.position) < _dectectionRange)
+                    {
+                        Debug.Log("Player is in range");
+
+                        //Change to chase state
+                        _currentState = EnemyState.CHASE;
+                    }
+
+
+
+                    if (Vector2.Distance(transform.position, _moveSpots[_randomSpot].position) < 0.2f)
+                    {
+                        if (_waitTime <= 0)
+                        {
+                            _randomSpot = UnityEngine.Random.Range(0, _moveSpots.Length);
+                            _waitTime = _startWaitTime;
+                        }
+                        else
+                        {
+                            _waitTime -= Time.deltaTime;
+                        }
+
+
+                    }
+
+                    //Debug purposes
                     if (Keyboard.current.digit1Key.wasPressedThisFrame)
                     {
                         _currentState = EnemyState.CHASE;
                     }
 
 
-
                     break;
                 }
 
+                //This is when the enemy wants to chase the player
             case EnemyState.CHASE:
                 {
+
+                   
+                    //Stop moving towards the player if it reaches a certain distance
+                    if (Vector2.Distance(transform.position, _target.position) > 1.2f)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+                    }
+
+
+                    if (Vector2.Distance(transform.position, _target.position) > _dectectionRange)
+                    {
+                        Debug.Log("Player is not in range");
+                        _currentState = EnemyState.PATROL;
+                    }
+
+                    //Debug purposes
                     if (Keyboard.current.digit1Key.wasPressedThisFrame)
                     {
                         _currentState = EnemyState.IDLE;
@@ -140,5 +238,13 @@ public class EnemyBehavior : MonoBehaviour
        
     }
 
-  
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _dectectionRange);
+        Gizmos.color = Color.red;
+    }
+
+
+   
 }
